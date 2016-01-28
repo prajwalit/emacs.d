417c130cb45153d25b74619b79abc6ff94ab36ae
Prajwalit Bhopale <prajwalit@helpshift.com>
2016-01-28 22:33:33 +0530

    Initial commit.

---
+++ init.el
@@ -0,0 +1,4 @@
+(setq user-full-name "Prajwalit Bhopale")
+(setq user-mail-address "contact@prajwalit.com")
+
+(load-file "~/.emacs.d/interface.el")
---
+++ interface.el
@@ -0,0 +1,7 @@
+;; Remove menubar, toolbar, scrollbar 
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Maximize frame
+(toggle-frame-maximized)
