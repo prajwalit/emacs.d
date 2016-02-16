a96f06e290bf8fa83bf60f9e7f5248cbb4d46efe
Prajwalit Bhopale <prajwalit@helpshift.com>
2016-02-16 16:05:43 +0530

    Fix backup folders.

--- init.el
+++ init.el
@@ -1,13 +1,18 @@
 (setq user-full-name "Prajwalit Bhopale")
 (setq user-mail-address "contact@prajwalit.com")
 
+;; Setup folders
 (setq root-dir (file-name-directory
                 (or (buffer-file-name) load-file-name)))
 
 (setq config-dir (file-name-as-directory (concat root-dir "configs")))
 (setq tmp-dir (file-name-as-directory (concat root-dir "tmp")))
-(make-directory tmp-dir t)
+(setq backup-directory-alist `(("." . ,(expand-file-name
+                                        (concat tmp-dir "backups")))))
+(setq auto-save-file-name-transforms `((".*" ,(expand-file-name
+                                               (concat tmp-dir "backups")))))
 
+;; Start
 (load-file (concat root-dir "packages.el"))
 (load-file (concat root-dir "interface.el"))
 (load-file (concat root-dir "key-bindings.el"))
