ce63ec1b6931ed218d0c421638d68c512b1487dd
Prajwalit Bhopale <prajwalit@helpshift.com>
2016-02-17 22:46:04 +0530

    Move git-path, meta change under mac check.

--- system.el
+++ system.el
@@ -1,7 +1,5 @@
-(setq magit-git-executable "/usr/local/bin/git"
-      vc-git-program "/usr/local/bin/git")
-
-(setq mac-command-modifier 'meta)
-
 (when (memq window-system '(mac ns))
+  (setq magit-git-executable "/usr/local/bin/git"
+        vc-git-program "/usr/local/bin/git"
+        mac-command-modifier 'meta)
   (exec-path-from-shell-initialize))
