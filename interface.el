a5594de8711c4f35a9a2e277ad8b583c3819162a
Prajwalit Bhopale <prajwalit@helpshift.com>
2016-02-17 20:40:21 +0530

    Add column-number.

--- interface.el
+++ interface.el
@@ -1,7 +1,8 @@
-;; Remove menubar, toolbar, scrollbar 
+;; Remove menubar, toolbar, scrollbar
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(column-number-mode t)
 
 ;; Maximize frame
 (toggle-frame-maximized)
