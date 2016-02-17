12b646429813cd80f3cbfa1c7053f1e3eb92ba60
Prajwalit Bhopale <prajwalit@helpshift.com>
2016-02-17 22:46:49 +0530

    Enable web-mode for html, css files.

--- configs/web-mode.el
+++ configs/web-mode.el
@@ -30,3 +30,5 @@
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.scss$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
