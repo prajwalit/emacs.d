12d85fadb65db98fd7d58bbc46501ab62f2a81af
Prajwalit Bhopale <prajwalit@helpshift.com>
2016-02-25 17:39:23 +0530

    Add sudo-find-file fn.

---
+++ utils.el
@@ -0,0 +1,5 @@
+(defun sudo-find-file (file-name)
+  "Like find file, but opens the file as root."
+  (interactive "FSudo Find File: ")
+  (let ((tramp-file-name (concat "/sudo::" (expand-file-name file-name))))
+    (find-file tramp-file-name)))
